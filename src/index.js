539f43571e8be87f55708f7cea6a4828b9a28d7c
Ryan A. Booth <rbooth@entelo.com>
2016-11-17 10:40:08 -0800

    search bar added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,39 @@
-import React from 'react';
+import _ from 'lodash';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/app';
+import YTSearch from 'youtube-api-search';
+const API_KEY = 'AIzaSyADduhTyVL5DNdVxzjrt-ydSTbbvmEKxZ8';
+import SearchBar from './components/SearchBar';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+
+    this.videoSearch('React js');
+  }
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      })
+    });
+  }
+
+  render() {
+    const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
+
+    return (
+      <div>
+        <SearchBar onSearchTermChange={videoSearch} />
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector('.container'));
